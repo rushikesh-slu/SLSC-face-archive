685037c7d5f7c04ffe07d560e5a3e2ef08b89067
rushikesh-slu <rushikesh.nalamothu@slu.edu>
2023-07-22 20:52:20 -0500

    setting up componenets

---
+++ frontend/SLSCFaceTagging/src/app/person-gallery/person-gallery.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-gallery',
+  templateUrl: './person-gallery.component.html',
+  styleUrls: ['./person-gallery.component.css']
+})
+export class PersonGalleryComponent {
+
+}
