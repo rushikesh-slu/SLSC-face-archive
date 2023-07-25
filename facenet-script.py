ac75b10d3f95dace5eb692011056a2a16dc5f03c
Blessing U. Effiong <1657-beffiong1@users.noreply.git.cs.slu.edu>
2023-07-25 10:18:34 -0500

    complete facenet

--- facenet-script.py
+++ facenet-script.py
@@ -9,8 +9,10 @@ from facenet_pytorch import MTCNN, InceptionResnetV1
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 import ssl
+import uuid
 
 ssl._create_default_https_context = ssl._create_unverified_context
+
 # Function to load an image from file and preprocess it
 def load_image(image_path):
     img = Image.open(image_path).convert('RGB')
@@ -20,6 +22,7 @@ def load_image(image_path):
 # Function to get face embeddings from an image
 def get_face_embedding(img, resnet):
     return resnet(img.unsqueeze(0)).detach().numpy()[0]
+
 # Replace 'path_to_image_folder' with the path to your image folder
 image_folder = '/Users/beffiong/desktop/YES JPEGs'
 output_csv = 'output.csv'
@@ -42,6 +45,10 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
 ])
+
+# Create a dictionary to store UUIDs as keys and a list of image names as values
+uuid_dict = {}
+
 # Process each image and extract face embeddings
 for image_path in image_files:
     try:
@@ -49,24 +56,35 @@ for image_path in image_files:
         face_embedding = get_face_embedding(img, resnet)
         embeddings.append(face_embedding)
         image_paths.append(image_path)
+
+        # Get the person's name (replace this with your logic to get the person's name from the image path)
+        person_name = os.path.basename(image_path).split('.')[0]
+
+        # Check if the person's UUID already exists in the dictionary
+        if person_name in uuid_dict:
+            # If it exists, add the image name to the existing UUID's list of images
+            uuid_dict[person_name].append(os.path.basename(image_path))
+        else:
+            # If it doesn't exist, generate a new UUID and create a new entry in the dictionary
+            new_uuid = str(uuid.uuid4())
+            uuid_dict[person_name] = [os.path.basename(image_path)]
     except Exception as e:
         print(f"Error processing image {image_path}: {e}")
 
 # Save the face embeddings and image paths to a CSV file
 with open(output_csv, 'w', newline='') as csvfile:
     csv_writer = csv.writer(csvfile)
-    csv_writer.writerow(['Image_Path', 'Embedding'])
-    for path, embedding in zip(image_paths, embeddings):
-        csv_writer.writerow([path, embedding])
-        
-        # Apply t-SNE to reduce the embeddings to 2D
+    csv_writer.writerow(['UUID', 'Image_Names'])
+    for person_name, image_list in uuid_dict.items():
+        csv_writer.writerow([person_name, ', '.join(image_list)])
+
+# Apply t-SNE to reduce the embeddings to 2D
 # Convert embeddings from a list of 1D arrays to a 2D array
 embeddings_2d_array = np.array(embeddings)
 
 # Apply t-SNE to reduce the embeddings to 2D
 tsne = TSNE(n_components=2, random_state=0)
 embeddings_2d = tsne.fit_transform(embeddings_2d_array)
-
 
 # Create a scatter plot of the 2D embeddings
 plt.figure(figsize=(8, 6))
