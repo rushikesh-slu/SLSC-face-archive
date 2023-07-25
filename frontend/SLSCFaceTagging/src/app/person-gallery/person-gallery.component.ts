2230fe2c852ca5cc8ce01d39a95e36cd2a8c124e
Manaswini <manaswinich.1208@gmail.com>
2023-07-25 11:07:51 +0530

    Adding_Person_Gallery_Features

--- frontend/SLSCFaceTagging/src/app/person-gallery/person-gallery.component.ts
+++ frontend/SLSCFaceTagging/src/app/person-gallery/person-gallery.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../services/api/api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-person-gallery',
@@ -7,4 +9,33 @@ import { Component } from '@angular/core';
 })
 export class PersonGalleryComponent {
 
+  constructor( private api: ApiService,private route: ActivatedRoute) { }
+
+  images: any[] = [];
+
+  personId :any;
+
+  ngOnInit(): void {
+
+    this.route.params.subscribe(params => {
+      this.personId = params['id'];
+      console.log(this.personId)
+      let payload={
+        'id':this.personId
+      }
+      this.api.getallPhotos(payload).subscribe(result =>{
+        let tempImgArray = result["lis"]
+        for(var img of tempImgArray){
+          let tempStr = 'assets/images/'+img
+          let temp = {
+            url: tempStr,
+          }
+          this.images.push(temp)
+        }
+      })
+
+    });
+
+    
+  }
 }
