dd5e5de01689f2dfa4ef383ecf0a7556d6f19f5d
rushikesh-slu <rushikesh.nalamothu@slu.edu>
2023-08-06 16:05:30 -0500

    Adding images

--- backend/main.py
+++ backend/main.py
@@ -1,28 +1,20 @@
-from flask import Flask, Blueprint, send_from_directory, render_template,request,make_response
+from flask import Flask, Blueprint, send_from_directory, request
 from flask_cors import CORS
-
 from api.user import user
+import os
 
-
-
-app = Flask(__name__, static_folder='')
+app = Flask(__name__, static_folder='./../frontend/SLSCFaceTagging/dist/slscface-tagging')
 CORS(app)
+
 app.register_blueprint(user,url_prefix='/api')
 
-
-@app.route('/assets/<path:filename>')
-def custom_static_for_assets(filename):
-    return send_from_directory('assets', filename)
-
-
-@app.route('/<path:filename>')
-def custom_static(filename):
-    return send_from_directory('', filename)
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
